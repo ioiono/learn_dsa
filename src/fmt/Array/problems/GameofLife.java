f23e0c8c992f5ca7759474221583ad6e7e8f5ca5
lty96117 <18642966+lty96117@users.noreply.github.com>
2018-10-06 17:14:18 +0800

    📦  NEW: game-of-life

---
+++ src/fmt/Array/problems/GameofLife.java
@@ -0,0 +1,20 @@
+package fmt.Array.problems;
+
+public class GameofLife {
+    public void gameOfLife(int[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j) {
+                int lives = 0;
+                // Scan the 3x3 region including (j, i).
+                for (int y = Math.max(0, i - 1); y < Math.min(m, i + 2); ++y)
+                    for (int x = Math.max(0, j - 1); x < Math.min(n, j + 2); ++x)
+                        lives += board[y][x] & 1;
+                if (lives == 3 || lives - board[i][j] == 3) board[i][j] |= 0b10;
+            }
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                board[i][j] >>= 1;
+    }
+}
