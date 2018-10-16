ed24879ccf2fa48b58e1a3718a9385319b580f74
lty96117 <18642966+lty96117@users.noreply.github.com>
2018-10-16 12:29:58 +0800

    📦  NEW: rand

---
+++ src/fmt/Math/ImplementRand10UsingRand7.java
@@ -0,0 +1,30 @@
+package fmt.Math;
+
+public class ImplementRand10UsingRand7 {
+    // generates a uniform random integer in the range 1 to 7
+    private int rand7() {
+        return 1 + (int) (Math.random() * 7);
+    }
+
+    public int rand10() {
+        int index = Integer.MAX_VALUE;
+        while (index > 40) {
+            index = 7 * (rand7() - 1) + rand7();
+        }
+
+        return (index - 1) % 10 + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ImplementRand10UsingRand7().rand10());
+        System.out.println(new ImplementRand10UsingRand7().rand10());
+        System.out.println(new ImplementRand10UsingRand7().rand10());
+        System.out.println(new ImplementRand10UsingRand7().rand10());
+
+        System.out.println(new ImplementRand10UsingRand7().rand7());
+        System.out.println(new ImplementRand10UsingRand7().rand7());
+        System.out.println(new ImplementRand10UsingRand7().rand7());
+        System.out.println(new ImplementRand10UsingRand7().rand7());
+        System.out.println(new ImplementRand10UsingRand7().rand7());
+    }
+}
