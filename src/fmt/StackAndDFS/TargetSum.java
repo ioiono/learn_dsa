679c07acca738b7e2b385b221c8d817f9bfbd66f
lty96117 <18642966+lty96117@users.noreply.github.com>
2018-10-23 18:50:04 +0800

    📦  NEW: upd

--- src/fmt/StackAndDFS/TargetSum.java
+++ src/fmt/StackAndDFS/TargetSum.java
@@ -11,8 +11,27 @@ public class TargetSum {
                 dfs(nums, sum - nums[k], target, k + 1);
     }
 
+    // DP
+    // Runtime: 7 ms, faster than 100.00%.
+    public int findTargetSumWaysDP(int[] nums, int s) {
+        int sum = 0;
+        for (int n : nums)
+            sum += n;
+        return sum < s || (s + sum) % 2 > 0 ? 0 : subsetSum(nums, (s + sum) >>> 1);
+    }
+
+    public int subsetSum(int[] nums, int s) {
+        int[] dp = new int[s + 1];
+        dp[0] = 1;
+        for (int n : nums)
+            for (int i = s; i >= n; i--)
+                dp[i] += dp[i - n];
+        return dp[s];
+    }
+
     public static void main(String[] args) {
         TargetSum ts = new TargetSum();
         System.out.println(ts.findTargetSumWays(new int[]{1, 1, 1, 1, 1}, 3));
+        System.out.println(ts.findTargetSumWaysDP(new int[]{1, 1, 1, 1, 1}, 3));
     }
 }
