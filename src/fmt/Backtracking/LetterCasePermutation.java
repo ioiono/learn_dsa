9635df0dd6a04381b92194ef5cdb8d5e60bd6e50
lty96117 <18642966+lty96117@users.noreply.github.com>
2018-10-30 00:57:28 +0800

    upd

--- src/fmt/Backtracking/LetterCasePermutation.java
+++ src/fmt/Backtracking/LetterCasePermutation.java
@@ -1,6 +1,7 @@
 package fmt.Backtracking;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LetterCasePermutation {
@@ -11,25 +12,47 @@ public class LetterCasePermutation {
     }
 
     private void dfs(char[] chars, List<String> res, int start) {
+
+        Utils.indent(start);
+        System.out.println("dfs(" + new String(chars) + ", " + res + ", " + start + ")");
         if (start == chars.length) {
-            res.add(String.valueOf(chars));
+            /****** DEBUG *******/
+            System.out.println("Add => " + new String(chars));
+            System.out.println("========================================================================================================");
+            /****** DEBUG *******/
+
+            res.add(new String(chars));
             return;
         }
+
         dfs(chars, res, start + 1);
 
         if (Character.isDigit(chars[start])) return;
         chars[start] ^= 32;
         dfs(chars, res, start + 1);
-        chars[start] ^= 32;
+        //        chars[start] ^= 32;
     }
 
+    public List<String> letterCasePermutation2(String S) {
+        List<String> ans = new ArrayList<>(Arrays.asList(S));
+        System.out.println(ans);
+        for (int i = 0; i < S.length(); ++i) { // Traverse string S char by char.
+            for (int j = 0, sz = ans.size(); S.charAt(i) > '9' && j < sz; ++j) { // S.charAt(i) > '9': letter, not digit.
+                char[] ch = ans.get(j).toCharArray(); // transform to char[] the string @ j of ans.
+                ch[i] ^= (1 << 5); // toggle case of charAt(i).
+                ans.add(String.valueOf(ch)); // append to the end of ans.
+            }
+        }
+        return ans;
+    }
 
     public static void main(String[] args) {
         LetterCasePermutation lcp = new LetterCasePermutation();
         System.out.println(lcp.letterCasePermutation("ab"));
-        System.out.println(lcp.letterCasePermutation(""));
+        //        System.out.println(lcp.letterCasePermutation(""));
         System.out.println(lcp.letterCasePermutation("a1b2"));
         System.out.println(lcp.letterCasePermutation2("a1b2"));
+        //        System.out.println(lcp.letterCasePermutation("a1b2c3"));
     }
 
 
---
+++ src/fmt/Backtracking/Utils.java
@@ -0,0 +1,9 @@
+package fmt.Backtracking;
+
+public class Utils {
+    static void indent(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print("    ");
+        }
+    }
+}
