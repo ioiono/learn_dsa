41603ba5d20ec41a765634f31db29c8c32860903
lty96117 <18642966+lty96117@users.noreply.github.com>
2018-10-30 21:27:54 +0800

    upd

--- src/fmt/DynamicProgramming/UniquePaths.java
+++ src/fmt/DynamicProgramming/UniquePaths.java
@@ -4,19 +4,19 @@ import java.util.Arrays;
 
 public class UniquePaths {
     public int uniquePaths(int m, int n) {
-        int[][] map = new int[m][n];
+        int[][] dp = new int[m][n];
         for (int i = 0; i < m; i++) {
-            map[i][0] = 1;
+            dp[i][0] = 1;
         }
-        for (int j = 0; j < n; j++) {
-            map[0][j] = 1;
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
         }
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                map[i][j] = map[i - 1][j] + map[i][j - 1];
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
-        return map[m - 1][n - 1];
+        return dp[m - 1][n - 1];
     }
 
     // time : O(n * m) space : O(n) actually could be O(min(n,m))
@@ -53,7 +53,7 @@ public class UniquePaths {
         return (int) res;
     }
 
-    public static void main(String[]  args) {
+    public static void main(String[] args) {
         System.out.println(new UniquePaths().uniquePaths(3, 3));
         System.out.println(new UniquePaths().uniquePaths2(4, 3));
         System.out.println(new UniquePaths().uniquePathsMath(3, 3));
