57bbe53d6f4559c129bd6a07339346bb9d310089
lty96117 <18642966+lty96117@users.noreply.github.com>
2019-05-02 01:27:04 +0800

    📦  NEW: update

--- src/fmt/Array/problems/DailyCode224.java
+++ src/fmt/Array/problems/DailyCode224.java
@@ -8,10 +8,21 @@ package fmt.Array.problems;
  * For example, for the input [1, 2, 3, 10], you should return 7.
  * <p>
  * Do this in O(N) time.
- *
+ * <p>
  * Trick one...
  */
 public class DailyCode224 {
+    /**
+     * what is the smallest positive integer you cannot make using the first k elements of the array? You then scan
+     * forward through the array from left to right, updating your answer to this question, until you find the smallest
+     * number you can't make.
+     *
+     * from https://stackoverflow.com/a/21078133
+     *
+     * @param nums sorted array
+     *
+     * @return smallest num cannot be made
+     */
     public int smallest(int[] nums) {
         int res = 1; // Initialize result
         // Traverse the array and increment 'res' if arr[i] is
@@ -24,7 +35,7 @@ public class DailyCode224 {
 
     public static void main(String[] args) {
         DailyCode224 dc = new DailyCode224();
-        System.out.println(dc.smallest(new int[]{1, 2, 3, 10}));
+        System.out.println(dc.smallest(new int[]{1, 3, 10}));
 
     }
 }
