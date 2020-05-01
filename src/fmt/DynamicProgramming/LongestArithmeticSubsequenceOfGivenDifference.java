d3679d30964f7588b41735e7afc26821682e2259
indefinitus <18642966+indefinitus@users.noreply.github.com>
2020-05-01 17:34:21 +0800

    test

---
+++ src/fmt/DynamicProgramming/LongestArithmeticSubsequenceOfGivenDifference.java
@@ -0,0 +1,5 @@
+package fmt.DynamicProgramming;
+
+public class LongestArithmeticSubsequenceOfGivenDifference {
+
+}
---
+++ src/fmt/Impl/AThanosSort1145A.java
@@ -0,0 +1,7 @@
+package fmt.Impl;
+
+public class AThanosSort1145A {
+    
+    public static void main(String[] args) {
+    }
+}
